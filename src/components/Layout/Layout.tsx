29becacc84ebe7e4e06bd9860102429881659b03
Enes <enes.sehic@hotmail.fi>
2021-01-11 12:12:29 +0200

    Insert StarBackground into Layout.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import StarBackground from './StarBackground/StarBackground';
+
 const Layout = () => {
     return (
         <>
-            <p>Works</p>
+            <StarBackground />
         </>
     );
 };
