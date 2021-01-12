7ac25e07f05d47bf068d8ae7b6d3629f6719b21b
Enes <enes.sehic@hotmail.fi>
2021-01-12 14:15:33 +0200

    Initial content for Home.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,7 +5,21 @@ import Container from '../components/Container/Container';
 const Home = () => {
     return (
         <Container>
-            <p>Yo</p>
+            <p>Be welcomed in this place!</p>
+            <br />
+            <h3>...What <em>is</em> this place?</h3>
+            <br />
+            <p>This is a little personal project made for two reasons:</p>
+            <p className={'indented'}>
+                1. To finally develop something of my own instead of watching endless guides in React development.
+                I&apos;ve been feeling anxious about not having anything to show for the time spent learning this JavaScript library - except of course the cookie-cutter to-do lists and such.
+                Those are all well and good for learning a new technology, of course, but if I am to create something for my &lsquo;portfolio&lsquo;, I want it to be something more personal.
+            </p>
+            <p className={'indented'}>
+                2. To acknowledge a game that really doesn&lsquo;t seem to get enough recognition online. What seems to contribute to this a lot is the fact that Outer Wilds was released
+                in the same year as another, more talked about game, Outer Worlds.
+            </p>
+            <p>Release May 2019</p>
         </Container>
     );
 };
