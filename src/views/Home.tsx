0032fa71de835a8e4476beb90673185bb2e8b17d
Enes <enes.sehic@hotmail.fi>
2021-01-12 11:12:36 +0200

    Created an initial Home view.

---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Container from '../components/Container/Container';
+
+const Home = () => {
+    return (
+        <Container>
+            <p>Yo</p>
+        </Container>
+    );
+};
+
+export default Home;
