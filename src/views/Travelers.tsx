69860090e6e779dbea9ba1610bb9d0b00f54e67f
Enes <enes.sehic@hotmail.fi>
2021-01-12 14:36:18 +0200

    Created the view Travelers and gave it some preliminary content.

---
+++ src/views/Travelers.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Container from '../components/Container/Container';
+
+const Travelers = () => {
+    return (
+        <Container>
+            <p>
+                As you traverse the star system, you&apos;ll likely run into other other members of Outer Wilds Ventures. These encounters are moments
+                of calm amidst all the chaos of your exploration of the system, so do take the opportunity to relax a bit. Sit by the campires, roast some marshmallows
+                and have conversations with your fellow Hearthians. They&apos;re always interested in your discoveries throughout the solar system, but they&apos;re also
+                happy enough to share their own findings with you.
+            </p>
+        </Container>
+    );
+};
+
+export default Travelers;
