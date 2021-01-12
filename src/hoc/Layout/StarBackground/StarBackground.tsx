ab51a3d1dfea1f4f5df9073f4464c1942ceace9f
Enes <enes.sehic@hotmail.fi>
2021-01-12 16:20:41 +0200

    Tsparticles' options are now stored in a json file.

--- src/hoc/Layout/StarBackground/StarBackground.tsx
+++ src/hoc/Layout/StarBackground/StarBackground.tsx
@@ -1,58 +1,12 @@
 import React from 'react';
-import Particles from 'react-tsparticles';
+import Particles, { ISourceOptions } from 'react-tsparticles';
+
+import particlesOptions from './particles.json';
 
 const StarBackground = () => {
 
-    const options = {
-        background: {
-            color: {
-                value: '#000'
-            }
-        },
-        fpsLimit: 60,
-        particles: {
-            color: {
-                value:'#fff'
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 1080
-                },
-                limit: 0,
-                value: 100
-            },
-            opacity: {
-                animation: {
-                    enable: true,
-                    minimumValue: 0.05,
-                    speed: 0.25,
-                    sync: false
-                },
-                random: {
-                    enable: true,
-                    minimumValue: 0.05
-                },
-                value: 1
-            },
-            shape: {
-                type: 'circle'
-            },
-            size: {
-                random: {
-                    enable: true,
-                    minimumValue: 0.5
-                },
-                value: 2
-            }
-        }
-    };
-
     return (
-        <Particles
-            height={window.outerHeight}
-            options={options}
-        />
+        <Particles options={particlesOptions as ISourceOptions} />
     );
 };
 
