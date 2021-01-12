74f718894732c0baff710ef06e203a62443712d0
Enes <enes.sehic@hotmail.fi>
2021-01-12 11:27:06 +0200

    Render the Home view for path '/'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
+import Home from './views/Home';
 
 const App = () => {
+    const routes = [
+        <Route key="home" path="/" component={Home} strict />
+    ];
+
     return (
-        <Layout />
+        <Router>
+            <Layout>
+                <Switch>
+                    {routes}
+                </Switch>
+            </Layout>
+        </Router>
     );
 };
 
--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import StarBackground from './StarBackground/StarBackground';
 import Navbar from '../../components/Navigation/Navbar/Navbar';
+import classes from './Layout.module.css';
 
-const Layout = () => {
+interface LayoutProps {
+    children: ReactNode
+}
+
+const Layout = (props: LayoutProps) => {
     return (
         <>
             <StarBackground />
             <Navbar />
+            <main className={classes.content}>
+                {props.children}
+            </main>
         </>
     );
 };
