c5814502056c089f8bbae08150e8b970fde5c82a
Enes <enes.sehic@hotmail.fi>
2021-01-12 14:51:45 +0200

    Created a Spacer component.

---
+++ src/components/Spacer/Spacer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface SpacerProps {
+    width: string
+    height: string
+}
+
+const Spacer = (props: SpacerProps) => {
+    return (
+        <div style={{width: `${props.width}`, height: `${props.height}`}}>
+            &nbsp;
+        </div>
+    );
+};
+
+export default Spacer;
