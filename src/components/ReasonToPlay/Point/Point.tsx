63e26048045bd46a01e6783f35b473d66bd866a8
Enes <enes.sehic@hotmail.fi>
2021-01-26 15:37:27 +0200

    Created a Point component for reason to play.

---
+++ src/components/ReasonToPlay/Point/Point.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from './Point.module.css';
+import { IReasonToPlay } from '../../../types';
+
+interface PointProps {
+    pointClicked: () => void,
+    reason: IReasonToPlay,
+    showExplanation: boolean
+}
+
+const Point = ({pointClicked, reason, showExplanation}: PointProps) => {
+    /**
+     * Show a downwards facing arrow when explanation isn't opened, upwards facing when it is open.
+     */
+    const displayArrow = () => showExplanation ? String.fromCharCode(0x25B2) : String.fromCharCode(0x25BC);
+
+    return (
+        <p
+            className={[classes.reasonContent, classes.point].join(' ')}
+            onClick={pointClicked}
+        >
+            <span>{`${reason.reasonNum}. ${reason.point}`}</span>
+
+            <span className={classes.arrowSpan}>
+                {
+                    reason.explanation && displayArrow()
+                }
+            </span>
+        </p>
+    );
+};
+
+export default Point;
