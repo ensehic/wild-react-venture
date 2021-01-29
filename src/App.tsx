0ec8f8d9234d650f1edd4502482a9ab6f2ff072f
Enes <enes.sehic@hotmail.fi>
2021-01-29 12:12:20 +0200

    Added transitions between views.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
@@ -9,21 +10,39 @@ import TheWorld from './views/TheWorld/TheWorld';
 import Travelers from './views/Travelers/Travelers';
 
 const App = () => {
+    const location = useLocation(); // Location being routed to
+
     const routes = [
         <Route key="home" path="/" component={Home} exact />,
-        <Route key="home" path="/reasons-to-play" component={ReasonsToPlay} />,
+        <Route key="reasons" path="/reasons-to-play" component={ReasonsToPlay} />,
         <Route key="the-world" path="/the-world" component={TheWorld} />,
         <Route key="travelers" path="/travelers" component={Travelers} />
     ];
 
+    const transitions = useTransition(location, location => location.pathname, {
+        from: {
+            position: 'absolute',
+            width: '100%',
+            opacity: 0,
+            transform: 'translateX(100%)'
+        },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-50%)' }
+    });
+
     return (
-        <Router>
-            <Layout>
-                <Switch>
-                    {routes}
-                </Switch>
-            </Layout>
-        </Router>
+        <Layout>
+            {transitions.map(({ item, props, key }) => (
+                <animated.div key={key} style={props}>
+                    <Switch location={item}>
+                        {routes}
+                    </Switch>
+                </animated.div>
+            ))}
+            {/* <Switch>
+                {routes}
+            </Switch> */}
+        </Layout>
     );
 };
 
