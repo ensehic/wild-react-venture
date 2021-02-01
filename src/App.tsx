9bfeea1c3efbb19f1bb7f611fe944d5421effa42
Enes <enes.sehic@hotmail.fi>
2021-02-01 13:51:56 +0200

    Use CSSTransition to animate the transitions between views.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Switch, Route, useLocation } from 'react-router-dom';
-import { useTransition, animated } from 'react-spring';
+import { Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
@@ -10,38 +10,36 @@ import TheWorld from './views/TheWorld/TheWorld';
 import Travelers from './views/Travelers/Travelers';
 
 const App = () => {
-    const location = useLocation(); // Location being routed to
-
     const routes = [
-        <Route key="home" path="/" component={Home} exact />,
-        <Route key="reasons" path="/reasons-to-play" component={ReasonsToPlay} />,
-        <Route key="the-world" path="/the-world" component={TheWorld} />,
-        <Route key="travelers" path="/travelers" component={Travelers} />
+        {path: '/', name: 'Home', View: Home},
+        {path: '/reasons-to-play', name: 'ReasonsToPlay', View: ReasonsToPlay},
+        {path: '/the-world', name: 'TheWorld', View: TheWorld},
+        {path: '/travelers', name: 'Travelers', View: Travelers}
     ];
-
-    const transitions = useTransition(location, location => location.pathname, {
-        from: {
-            position: 'absolute',
-            width: '100%',
-            opacity: 0,
-            transform: 'translateX(100%)'
-        },
-        enter: { opacity: 1, transform: 'translateX(0%)' },
-        leave: { opacity: 0, transform: 'translateX(-50%)' }
-    });
 
     return (
         <Layout>
-            {transitions.map(({ item, props, key }) => (
-                <animated.div key={key} style={props}>
-                    <Switch location={item}>
-                        {routes}
-                    </Switch>
-                </animated.div>
-            ))}
-            {/* <Switch>
-                {routes}
-            </Switch> */}
+            {
+                routes.map(({ path, View }) => (
+                    <Route key={path} exact path={path}>
+                        {/* React Router passes the object containing a match object, which exists if the route matches, otherwise it's null.
+                            This enables us to manage the in prop of CSSTransition based on the presence of match. */}
+                        {/* Animate the transitions between routes. Styling is defined in App.css */}
+                        {({ match }) => (
+                            <CSSTransition
+                                in={match !== null}
+                                timeout={600}
+                                classNames="view"
+                                unmountOnExit
+                            >
+                                <div className="view">
+                                    <View />
+                                </div>
+                            </CSSTransition>
+                        )}
+                    </Route>
+                ))
+            }
         </Layout>
     );
 };
