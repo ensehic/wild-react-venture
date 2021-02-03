c289647bdaadb71beeb652aa5f0e4a999608bce9
Enes <enes.sehic@hotmail.fi>
2021-02-03 13:08:41 +0200

    Rewrote NavigationItems so that it iterates over the route array to generate each NavigationItem.

--- src/components/Navigation/NavigationItems/NavigationItems.tsx
+++ src/components/Navigation/NavigationItems/NavigationItems.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.module.css';
+import routes from '../../../shared/appRoutes';
 
 const NavigationItems = () => (
     <ul className={classes.navigationItems}>
-        <NavigationItem text="Home" target="/" exact />
-        <NavigationItem text="10 Reasons to Play" target="/reasons-to-play" />
-        <NavigationItem text="The World of Outer Wilds" target="/the-world" />
-        <NavigationItem text="Travelers" target="/travelers" />
+        {
+            routes.map(
+                route => <NavigationItem
+                    key={route.path}
+                    text={route.name}
+                    target={route.path}
+                    exact
+                />
+            )
+        }
     </ul>
 );
 
