6f40e3dafe99c80afdd83965aef8ddc3827dc6aa
Enes <enes.sehic@hotmail.fi>
2021-02-03 13:05:09 +0200

    Use the route interface to define a type for routes.

--- src/shared/appRoutes.ts
+++ src/shared/appRoutes.ts
@@ -2,11 +2,12 @@ import Home from '../views/Home';
 import ReasonsToPlay from '../views/ReasonsToPlay';
 import TheWorld from '../views/TheWorld/TheWorld';
 import Travelers from '../views/Travelers/Travelers';
+import { IAppRoute } from '../types';
 
-const routes = [
+const routes: IAppRoute[] = [
     {path: '/', name: 'Home', View: Home},
-    {path: '/reasons-to-play', name: 'ReasonsToPlay', View: ReasonsToPlay},
-    {path: '/the-world', name: 'TheWorld', View: TheWorld},
+    {path: '/reasons-to-play', name: '10 Reasons to Play', View: ReasonsToPlay},
+    {path: '/the-world', name: 'The World of Outer Wilds', View: TheWorld},
     {path: '/travelers', name: 'Travelers', View: Travelers}
 ];
 
