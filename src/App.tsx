62cc0ae13728b2900547c2449cca8b2c88b8bbdf
Enes <enes.sehic@hotmail.fi>
2021-02-03 13:43:00 +0200

    Use CSSTransition's callbacks to decide whether to use .view or not.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
@@ -7,22 +7,33 @@ import Layout from './hoc/Layout/Layout';
 import routes from './shared/appRoutes';
 
 const App = () => {
+    const [viewClassName, setViewClassName] = useState<string|undefined>(undefined);
+
     return (
         <Layout>
             {
                 routes.map(({ path, View }) => (
                     <Route key={path} exact path={path}>
+
+                        {/* Animate the transitions between routes. Styling is defined in App.css */}
                         {/* React Router passes the object containing a match object, which exists if the route matches, otherwise it's null.
                             This enables us to manage the in prop of CSSTransition based on the presence of match. */}
-                        {/* Animate the transitions between routes. Styling is defined in App.css */}
+
                         {({ match }) => (
                             <CSSTransition
                                 in={match !== null}
                                 timeout={600}
-                                classNames="view"
+                                /**
+                                 * .view positions the div absolutely, which would mess with the site layout if it were kept like that
+                                 *  at all times. Using CSSTransition's callbacks, set .view only when needed.
+                                 */
+                                onEnter={() => setViewClassName('view')}
+                                onEntered={() => setViewClassName(undefined)}
+                                onExit={() => setViewClassName('view')}
+                                classNames={viewClassName}
                                 unmountOnExit
                             >
-                                <div className="view">
+                                <div className={viewClassName}>
                                     <View />
                                 </div>
                             </CSSTransition>
